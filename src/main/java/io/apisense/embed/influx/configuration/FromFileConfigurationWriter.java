c5662d63d592730c67ccf6eabcfc289c4f4f9aa5
Antoine Veuiller <aveuiller@gmail.com>
2018-03-09 15:25:28 +0100

    feat: Add a FromFileConfigurationWriter (#12)
    Closes #8

---
+++ src/main/java/io/apisense/embed/influx/configuration/FromFileConfigurationWriter.java
@@ -0,0 +1,63 @@
+package io.apisense.embed.influx.configuration;
+
+import java.io.File;
+import java.util.Map;
+
+/**
+ * Implementation of a {@link ConfigurationWriter} to use an existing configuration file.
+ */
+public final class FromFileConfigurationWriter implements ConfigurationWriter {
+    private final File configurationFile;
+    private final File dataPath;
+
+    FromFileConfigurationWriter(File configurationFile, File dataPath) {
+        this.configurationFile = configurationFile;
+        this.dataPath = dataPath;
+    }
+
+    @Override
+    public File getDataPath() {
+        return dataPath;
+    }
+
+    @Override
+    public void addStatements(Map<String, Object> config) {
+        throw new UnsupportedOperationException("The given configuration file is immutable");
+    }
+
+    @Override
+    public File writeFile() {
+        return configurationFile;
+    }
+
+    /**
+     * Build an {@link FromFileConfigurationWriter}.
+     */
+    public static final class Builder {
+        private final File configuration;
+        private File dataPath = null;
+
+        public Builder(File configurationFile) {
+            configuration = configurationFile;
+        }
+
+        public FromFileConfigurationWriter build() {
+            return new FromFileConfigurationWriter(configuration, dataPath);
+        }
+
+        /**
+         * Set the path to remove on server stop,
+         * nothing will be removed if not specified.
+         *
+         * This can be used to remove the data between each database usage.
+         *
+         * @param dataPath - path to data location
+         * @return The current {@link Builder}.
+         */
+        public Builder removePathOnStop(File dataPath) {
+            this.dataPath = dataPath;
+            return this;
+        }
+
+    }
+}
---
+++ src/test/java/io/apisense/embed/influx/configuration/FromFileConfigurationWriterTest.java
@@ -0,0 +1,43 @@
+package io.apisense.embed.influx.configuration;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.util.HashMap;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class FromFileConfigurationWriterTest {
+
+    @Test
+    public void TestTheFileIsReturned() throws Exception {
+        File configFile = new File("/tmp/my-config-file");
+        ConfigurationWriter defaultConfig = new FromFileConfigurationWriter.Builder(configFile).build();
+
+        assertThat("We should have the same File", defaultConfig.writeFile(), is(configFile));
+        assertThat("We don't have dataPath to remove by default", defaultConfig.getDataPath(), is(nullValue()));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void TestWeCantUseAddStatement() throws Exception {
+        File configFile = new File("/tmp/my-config-file");
+        ConfigurationWriter defaultConfig = new FromFileConfigurationWriter.Builder(configFile).build();
+
+        defaultConfig.addStatements(new HashMap<String, Object>());
+    }
+
+    @Test
+    public void TestWeCanSetADataPathToRemove() throws Exception {
+        File configFile = new File("/tmp/my-config-file");
+        File toRemove = new File("/tmp/my-data-path-to-remove");
+        ConfigurationWriter defaultConfig = new FromFileConfigurationWriter.Builder(configFile)
+                .removePathOnStop(toRemove)
+                .build();
+
+        assertThat("We should have the same File", defaultConfig.writeFile(), is(configFile));
+        assertThat("We have the set dataPath to remove", defaultConfig.getDataPath(), is(toRemove));
+    }
+
+}
